a2f6b2a17e9f427d809ab183d8396ca1da6280a7
Chillz <114379782+AZYsGithub@users.noreply.github.com>
2023-07-23 12:45:14 +0800

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,43 @@
+-- Load the DrRay library from the GitHub repository Library
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/Chillz-s-scripts/main/DrRay.lua"))()
+
+-- Create a new window and set its title and theme
+local window = DrRayLibrary:Load("DrRay!", "Default")
+
+-- Create the first tab with an image ID
+local tab1 = window.newTab("Tab 1", "ImageIdHere")
+
+-- Add elements to the first tab
+tab1.newLabel("Hello, this is Tab 1.")
+tab1.newButton("Button", "Prints Hello!", function()
+    print('Hello!')
+end)
+tab1.newToggle("Toggle", "Toggle! (prints the state)", true, function(toggleState)
+    if toggleState then
+        print("On")
+    else
+        print("Off")
+    end
+end)
+tab1.newInput("Input", "Prints your input.", function(text)
+    print("Entered text in Tab 1: " .. text)
+end)
+
+-- Create the second tab with a different image ID
+local tab2 = window.newTab("Tab 2", "ImageIdLogoHere")
+
+-- Add elements to the second tab
+tab2.newLabel("Hello, this is Tab 2.")
+tab2.newButton("Button", "Prints Hello!", function()
+    print('Hello!')
+end)
+tab2.newToggle("Toggle", "Toggle! (prints the state)", true, function(toggleState)
+    if toggleState then
+        print("On")
+    else
+        print("Off")
+    end
+end)
+tab2.newDropdown("Dropdown", "Select one of these options!", {"water", "dog", "air", "bb", "airplane", "wohhho", "yeay", "delete"}, function(selectedOption)
+    print(selectedOption)
+end)
